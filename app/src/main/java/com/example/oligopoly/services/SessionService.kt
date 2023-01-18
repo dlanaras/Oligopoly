fbb62e72ae471eae31f8772fa8810dd87c0b6279
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-01-18 09:31:12 +0100

    feat: added sessionservice functions (not yet functional)

--- app/src/main/java/com/example/oligopoly/services/SessionService.kt
+++ app/src/main/java/com/example/oligopoly/services/SessionService.kt
@@ -3,10 +3,19 @@ package com.example.oligopoly.services
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import com.example.oligopoly.models.Session
 
 class SessionService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    public fun saveSession(session: Session) {
+        TODO("Implement this")
+    }
+
+    public fun getSession(): Session {
+        TODO("Implement this")
     }
 }
