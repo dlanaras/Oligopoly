e1b48909ab1716c8cdc1aa808d4b7318d749d58e
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-01-18 11:41:52 +0100

    style: MainActivity.kt layout done

--- app/src/main/java/com/example/oligopoly/MainActivity.kt
+++ app/src/main/java/com/example/oligopoly/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.oligopoly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var startButton: Button
+    private lateinit var playButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton = findViewById(R.id.playButton);
+
+        playButton.text = "Play"
+
+        playButton.setOnClickListener() {
+            startActivity(Intent(this, GameSettingsActivity::class.java))
+        }
     }
 
     override fun onResume() {
