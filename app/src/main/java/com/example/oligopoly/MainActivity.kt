7f60f7f62acfbcf177b51adaf2acc1ade3bba67a
dlanaras <70207362+dlanaras@users.noreply.github.com>
2023-01-21 13:32:34 +0100

    feat: SessionActivity.kt implemented and working

--- app/src/main/java/com/example/oligopoly/MainActivity.kt
+++ app/src/main/java/com/example/oligopoly/MainActivity.kt
@@ -1,26 +1,53 @@
 package com.example.oligopoly
 
+import android.annotation.SuppressLint
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import com.example.oligopoly.services.SessionService
 
 class MainActivity : AppCompatActivity() {
     private lateinit var playButton: Button
+    private var mBound = false
+    private lateinit var sessionService: SessionService
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as SessionService.LocalBinder
+            sessionService = binder.getService()
+            mBound = true
+
+            if(sessionService.sessionExists()) {
+                startActivity(Intent(this@MainActivity, SessionActivity::class.java))
+            }
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playButton = findViewById(R.id.playButton);
+        Intent(this, SessionService::class.java).also { intent ->
+            startService(intent)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+
+        playButton = findViewById(R.id.playButton)
 
         playButton.text = "Play"
 
         playButton.setOnClickListener() {
             startActivity(Intent(this, GameSettingsActivity::class.java))
         }
-    }
-
-    override fun onResume() {
-        super.onResume()
     }
 }
--- app/src/main/java/com/example/oligopoly/SessionActivity.kt
+++ app/src/main/java/com/example/oligopoly/SessionActivity.kt
@@ -1,7 +1,12 @@
 package com.example.oligopoly
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
 import com.example.oligopoly.services.SessionService
 
@@ -9,19 +14,54 @@ class SessionActivity : AppCompatActivity() {
     private lateinit var sessionService: SessionService
     private lateinit var resumeButton: Button
     private lateinit var discardButton: Button
+    private var mBound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as SessionService.LocalBinder
+            sessionService = binder.getService()
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_session)
 
+        Intent(this, SessionService::class.java).also { intent ->
+            startService(intent)
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+
         resumeButton = findViewById(R.id.resumeButton)
         discardButton = findViewById(R.id.discardButton)
+
+        resumeButton.setOnClickListener {
+            resumeSession()
+        }
+
+        discardButton.setOnClickListener {
+            discardSessionAndNavigateToMainActivity()
+        }
     }
 
-    public fun discardSessionAndNavigateToMainActivity() {
-        TODO("implement this")
+    private fun discardSessionAndNavigateToMainActivity() {
+        if(mBound) {
+            sessionService.discardSession()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 
-    public fun resumeSession() {
-        TODO("implement this")
+    private fun resumeSession() {
+        if(mBound) {
+            val resumeIntent = Intent(this, InGameActivity::class.java)
+            resumeIntent.putExtra("resumeGame", true)
+
+            startActivity(resumeIntent)
+        }
     }
 }
